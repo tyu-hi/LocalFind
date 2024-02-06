984bfc2bc9850f205ea9f4b3aa3e2a96e4ab0ed6
naomi gong <naomigong2005@gmail.com>
2024-02-05 23:07:51 -0800

    I created the login and sign up page and connected it to firebase

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import {FIREBASE_AUTH} from "../firebase/firebase";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+
+
+const SignUp = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    //const [loading, setLoading] = useState(false);
+    const auth = FIREBASE_AUTH;
+
+    // const signup = async () => {
+    //     setLoading(true)
+    //     try{
+    //         const response = await createUserWithEmailAndPassword(auth, email, password);
+    //         console.log(response);
+    //     }
+    //     catch (error: any ){
+    //         console.log(error);
+    //         alert('Sign up failed: ' + error.message);
+    //     } finally {
+    //         setLoading(false);
+    //     }
+    // }
+
+    const signUp = (e: any) =>{
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email,password)
+        .then((userCredentials) => {
+            console.log(userCredentials);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        
+    };
+
+    return (
+        <div>
+            <form onSubmit = {signUp}>
+                <h1> Create An Account</h1>
+                <input type = "email" 
+                placeholder = "Enter your email" 
+                value= {email} 
+                onChange = {(e) => setEmail(e.target.value)}></input>
+                <input type = "password" 
+                placeholder = "Enter your password" 
+                value= {password} 
+                onChange = {(e) => setPassword(e.target.value)}></input>
+                <button type = "submit">Sign Up</button>
+            </form>
+        </div>
+
+    );
+};
+
+export default SignUp;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,49 +1,16 @@
-import * as React from 'react';
-import { useForm } from 'react-hook-form';
-import { Button, Form } from 'react-bootstrap';
+import SignIn from '../components/SignIn';
+import SignUp from '../components/Signup';
 
 const LoginPage = () => {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    // Add your authentication logic here
-    console.log('Logging in with:', data.username, data.password);
-  };
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>Login</h2>
-      <Form
-        style={{
-          width: '300px',
-          margin: 'auto',
-        }}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your username"
-            {...register('username', { required: 'Username is required' })}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter your password"
-            {...register('password', { required: 'Password is required' })}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+   <div className="LoginPage">
+      <SignIn />
+    
+    
+    <h3> If you do not have an account please make one!</h3> 
+    <SignUp />
     </div>
-  );
-};
+  )
+}
 
 export default LoginPage;
