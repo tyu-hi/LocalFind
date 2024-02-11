53f6832968352ff1174c14177f31d6a0f4494be8
Inman Costa <120146242+inmancosta@users.noreply.github.com>
2024-02-11 13:19:06 -0800

    added searchbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import AddRestuarant from './pages/AddRestaurant'
 import Navbar from './components/NavBar'
+import SearchBar from './components/SearchBar'
 //import { Navbar } from 'react-bootstrap'
 
 function App() {
   return (
       <div>
         <Navbar/>
+        <SearchBar/>
 
       </div>
   )
