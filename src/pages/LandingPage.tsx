df4271ac2a87c09faa5eb68022b6c210ded2c186
Inman Costa <120146242+inmancosta@users.noreply.github.com>
2024-02-11 20:02:32 -0800

    Update LandingPage.tsx

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import AddRestuarant from './pages/AddRestaurant'
+import Navbar from './components/NavBar'
+import SearchBar from './components/SearchBar'
+import Cards from './components/FeaturedLists'
+import Footer  from './components/Footer'
+import RecommendedRestaurants from './components/ReccomendedRestaurants'
+
+//import { Navbar } from 'react-bootstrap'
+
+function LandingPage() {
+  return (
+      <div>
+        <Navbar/>
+        <SearchBar/>
+        <Cards />
+        <RecommendedRestaurants />
+       <Footer />
+        
+      </div>
+  )
+}
+
+export default App
