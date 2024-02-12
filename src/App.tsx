3aa265243f645816693001d812beba48aa13a830
Inman Costa <120146242+inmancosta@users.noreply.github.com>
2024-02-11 20:00:53 -0800

    added reccommended restaurants, footer, and featured lists

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import LoginPage from './pages/LoginPage'
 import AddRestuarant from './pages/AddRestaurant'
 import Navbar from './components/NavBar'
 import SearchBar from './components/SearchBar'
+import Cards from './components/FeaturedLists'
+import Footer  from './components/Footer'
+import RecommendedRestaurants from './components/ReccomendedRestaurants'
+
 //import { Navbar } from 'react-bootstrap'
 
 function App() {
@@ -12,7 +16,10 @@ function App() {
       <div>
         <Navbar/>
         <SearchBar/>
-
+        <Cards />
+        <RecommendedRestaurants />
+       <Footer />
+        
       </div>
   )
 }
--- src/components/FeaturedLists.tsx
+++ src/components/FeaturedLists.tsx
@@ -1,20 +1,41 @@
 import * as React from 'react';
-import './FeaturedList.css'; // Import the CSS file
 
-function FeaturedList() {
+const FeaturedLists = () => {
+  // Sample random image links
+  const randomImageLinks = [
+    "https://tastesbetterfromscratch.com/wp-content/uploads/2023/06/Pepperoni-Pizza-1.jpg", // Example placeholder link
+    "https://st5.depositphotos.com/28053878/62733/i/1600/depositphotos_627332410-stock-photo-vertical-photo-sssorted-indian-food.jpg", // Example placeholder link
+    "https://www.modernfarmhouseeats.com/wp-content/uploads/2021/03/chili-lime-shrimp-ramen-2-scaled.jpg", // Example placeholder link
+  ];
+
   return (
-    <div className="featured-list">
-      <div className="box">
-        <a href="#">Featured List 1</a>
-      </div>
-      <div className="box">
-        <a href="#">Featured List 2</a>
-      </div>
-      <div className="box">
-        <a href="#">Featured List 3</a>
+    <div className="featured-lists">
+      <h2>Featured Lists</h2>
+      <div className="card-container">
+        <div className="card">
+          <img src={randomImageLinks[0]} alt="Cuisine 1" />
+          <div className="card-content">
+            <h3>Cuisine Category 1</h3>
+            <p>Description of Cuisine Category 1</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={randomImageLinks[1]} alt="Cuisine 2" />
+          <div className="card-content">
+            <h3>Cuisine Category 2</h3>
+            <p>Description of Cuisine Category 2</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={randomImageLinks[2]} alt="Cuisine 3" />
+          <div className="card-content">
+            <h3>Cuisine Category 3</h3>
+            <p>Description of Cuisine Category 3</p>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default FeaturedList;
+export default FeaturedLists;
