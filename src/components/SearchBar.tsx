ba83219db5911a68700a2a8a4c9ea15dca7a999d
tyu-hi <tyugo2122@gmail.com>
2024-02-12 20:24:23 -0800

    Added Tailwind CSS, file-naming edits

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 class SearchBar extends React.Component {
   render() {
     return (
-      
         <div className="bar">
           <input className="searchbar" placeholder="Find A Restaurant!" type="text" title="Search" />
         </div>
