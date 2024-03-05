4dfcca043711a4dcd31623a5b0472b5a11b33f0f
aparnahrn <aparnahariharan@ucla.edu>
2024-03-04 23:54:57 -0800

    Update SignUpPage.tsx

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,9 +1,34 @@
 //move sign up stuff here
+import NavBar from '../components/NavBar'
+import { userState } from "react";
+
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  const[action, setAction] = useState("Sign Up")
+
+
+ return (
+
+
+   <div>
+     <NavBar/>
+     <div className="header">
+       <div className="title">Welcome back.
+       Sign in to your account</div>
+       <form>
+         <input type="email" placeholder='example@example.com'/>
+         <input type="password" />
+         </form>
+       <div className='submit-container'>
+         <div className={action }>Sign In</div>
+         <div className='submit'>Sign Up</div>
+       </div>
+     </div>
+  
+   </div>
+ )
 }
 
+
 export default SignUpPage
+
