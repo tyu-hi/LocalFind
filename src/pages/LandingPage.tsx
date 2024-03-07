f56b24854109c085e1d9664c855566423d74f481
Inman Costa <120146242+inmancosta@users.noreply.github.com>
2024-03-07 14:57:07 -0800

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -19,9 +19,9 @@ const LandingPage = () => {
   };
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <h1 className="text-3xl mt-20 text-center text-primary font-serif display">
-      Unearth Local Flavors - Your Next Favorite Dish Awaits.
+        Unearth Local Flavors - Your Next Favorite Dish Awaits.
       </h1>
       <SearchBar onSubmit={handleSearchSubmit} />
 
@@ -32,8 +32,8 @@ const LandingPage = () => {
       </section>
 
       <footer className="mt-16 text-center p-4 bg-secondary-1 text-text-1 text-primary">
-    © 2024 Local Finds. All rights reserved.
-  </footer>
+        © 2024 Local Finds. All rights reserved.
+      </footer>
     </div>
   );
 };
