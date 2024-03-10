b5649e0e7b83e2703840c37dcbf01cb8b47f1700
tyu-hi <tyugo2122@gmail.com>
2024-03-09 21:28:55 -0800

    various styling

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,39 +1,50 @@
-//import AddRestuarantForm from './pages/AddRestaurantForm'
+import React from "react";
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 import FeaturedLists from "../components/FeaturedLists";
 import RecommendedRestaurants from "../components/RecommendedRestaurants";
 import { useNavigate } from "react-router-dom";
 import QuickAndAffordableDinners from "../components/QuickAndAffordableDinners";
-//import { useHistory } from 'react-router-dom'
-
-//import { Navbar } from 'react-bootstrap'
 
 const LandingPage = () => {
-  //get search query
   const navigate = useNavigate();
-  //const urlParams = new URLSearchParams(window.location.search);
-  //const query = urlParams.get('query') || '';
+
   const handleSearchSubmit = (searchValue: string) => {
     navigate(`/search?query=${searchValue}`);
   };
+
   return (
-    <div>
+    <div className="relative font-alata">
       <NavBar />
-      <h1 className="text-3xl mt-20 text-center text-primary font-serif display">
-        Unearth Local Flavors - Your Next Favorite Dish Awaits.
-      </h1>
-      <SearchBar onSubmit={handleSearchSubmit} />
 
-      <section className="mt-56">
-        <FeaturedLists />
-        {/*recommended restraunts here*/}
-        <QuickAndAffordableDinners />
-      </section>
+      {/* Background image */}
+      <div
+        className="absolute top-0 left-0 w-full h-3/6 bg-cover bg-center mb-10"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+          zIndex: -1,
+        }}
+      ></div>
 
-      <footer className="mt-16 text-center p-4 bg-secondary-1 text-text-1 text-primary">
-        © 2024 Local Finds. All rights reserved.
-      </footer>
+      <div className="relative z-10">
+        <h1 className="text-4xl mt-80 mb-10 text-center font-bold text-white text-primary drop-shadow-[0_3px_1.5px_rgba(0,0,0,.8)]">
+          Unearth Local Flavors - Your Next Favorite Dish Awaits
+        </h1>
+        <SearchBar onSubmit={handleSearchSubmit}/>
+
+        <section className="mt-96">
+          <FeaturedLists />
+          {/* recommended restaurants here */}
+          <div className="mt-20">
+            <QuickAndAffordableDinners />
+          </div>
+        </section>
+
+        <footer className="mt-16 text-center p-4 bg-secondary-1 text-text-1 text-primary">
+          © 2024 Local Finds. All rights reserved.
+        </footer>
+      </div>
     </div>
   );
 };
