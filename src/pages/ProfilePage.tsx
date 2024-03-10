d7fe2901a5761a2ba9b3055ce70d100bb5bd9184
tyu-hi <tyugo2122@gmail.com>
2024-03-09 23:14:03 -0800

    various styling, fixed profile pic bug

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -3,15 +3,13 @@ import NavBar from "../components/NavBar"
 //import { DefaultProfile } from "../svg-icons.tsx/DefaultProfile"
 import { useAuth, upload } from "../firebase/firebase";
 import { FIREBASE_AUTH, FIREBASE_FIRESTORE } from "../firebase/firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { addDoc, doc, collection, getDocs, query, where, setDoc } from "firebase/firestore";
 import { signOut } from 'firebase/auth';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
 import Map from '../components/Maps'
-import Playlists from "../components/Playlists"
-
-
+import { CiEdit } from "react-icons/ci";
+import Playlists from "../components/Playlists";
 //After use is logged in, I would like for the signup/sign in to disappear, 
 //and be replaced by the icon of the user
 
@@ -24,9 +22,43 @@ const ProfilePage = () => {
   /* HANDLE UPLOADING PROFILE IMAGE */
   const [photo, setPhoto] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
-  const [photoURL, setPhotoURL] = useState("");
+  const [photoURL, setPhotoURL] = useState("/Default_pfp.svg.png");
   //https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2c%2FDefault_pfp.svg%2F1200px-Default_pfp.svg.png&tbnid=t5PQpQ66IW5J4M&vet=12ahUKEwjApLK059GEAxWiJEQIHXLqB84QMygAegQIARBy..i&imgrefurl=https%3A%2F%2Fen.m.wikipedia.org%2Fwiki%2FFile%3ADefault_pfp.svg&docid=o_Ii_cyIO_p3fM&w=1200&h=1200&q=default%20profile%20picture%20&ved=2ahUKEwjApLK059GEAxWiJEQIHXLqB84QMygAegQIARBy
   
+  //Allow user to edit bio//////////////////////////////////////
+  const [isEditingBio, setIsEditingBio] = useState(false);
+  const [bioText, setBioText] = useState("");
+
+  /* HANDLE UPLOADING FIRST AND LAST NAME*/
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const firestore = FIREBASE_FIRESTORE;
+  const colRef = collection(firestore, "Users");
+  
+  // Function to toggle edit 
+  const toggleEditMode = () => {
+    setIsEditingBio(!isEditingBio);
+  };
+  const updateBio = async (newBioText: string) => {
+    if (currentUser) {
+      const userDocRef = doc(colRef, currentUser.uid); // Reference to the user's document
+      try {
+        await setDoc(userDocRef, { bioText: newBioText }, { merge: true }); // Update the bioText field
+        console.log('Bio updated successfully!');
+      } catch (error) {
+        console.error("Error updating bio text:", error);
+      }
+    }
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      updateBio(bioText);
+      setIsEditingBio(false); // Exit edit mode
+    }
+  };
+
+
+  //Handle image change////////////////////////
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files?.[0]) {
       const file = e.target.files[0];
@@ -47,7 +79,7 @@ const ProfilePage = () => {
       }
     }
   };
-
+  //image change
   const handleClick = () => {
     if (currentUser) {  
       if (photo)
@@ -60,12 +92,6 @@ const ProfilePage = () => {
    
   };
 
-  /* HANDLE UPLOADING FIRST AND LAST NAME*/
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const firestore = FIREBASE_FIRESTORE;
-  const colRef = collection(firestore, "Users");
-  
   //get user data and update name and profile image
   useEffect(() => {
     if (currentUser) {
@@ -77,7 +103,8 @@ const ProfilePage = () => {
               const data = doc.data();
               setFirstName(data.firstname);
               setLastName(data.lastname);
-              setPhotoURL(data.photoURL || '/public/Default_pfp.svg.png');
+              setPhotoURL(data.photoURL || 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png');
+              setBioText(data.bioText);
             });
           }
         })
@@ -99,12 +126,16 @@ const ProfilePage = () => {
     }
   };
 
+
+  //add bio
+
+
   
   return (
     <div className="font-alata">
         <NavBar/>
         {/* if user is logged in show this profile page, if not tell user to log in | conditional rendering*/}
-        <div className="max-w-xl mx-auto width-full">
+        <div className="max-w-2xl mx-auto width-full">
           
           <div className="flex flex-col items-center justfiy-center mb-10 mt-10 w-full">
             {/*top*/}
@@ -122,7 +153,7 @@ const ProfilePage = () => {
                 />
                 
                 {/* Profile picture */}
-                <img src={currentUser?.photoURL ?? 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2c%2FDefault_pfp.svg%2F1200px-Default_pfp.svg.png&tbnid=t5PQpQ66IW5J4M&vet=12ahUKEwjApLK059GEAxWiJEQIHXLqB84QMygAegQIARBy..i&imgrefurl=https%3A%2F%2Fen.m.wikipedia.org%2Fwiki%2FFile%3ADefault_pfp.svg&docid=o_Ii_cyIO_p3fM&w=1200&h=1200&q=default%20profile%20picture%20&ved=2ahUKEwjApLK059GEAxWiJEQIHXLqB84QMygAegQIARBy'} 
+                <img src={currentUser?.photoURL ?? '/Default_pfp.svg.png'} 
                   alt="Profile" 
                   className="avatar" 
                   title="Profile" //tooltip
@@ -132,7 +163,7 @@ const ProfilePage = () => {
                 <button
                   disabled={loading || !photo}
                   onClick={handleClick}
-                  className="absolute bottom-0 left-0 right-0 mx-auto bg-gray-900 bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center text-white text-xs font-medium cursor-pointer rounded-full p-2"
+                  className="absolute bottom-14 left-0 right-0 mx-auto bg-gray-900 bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center text-white text-xs font-medium rounded-xl cursor-pointer p-2"
                   style={{ width: 'fit-content' }}
                 >
                   Change Image
@@ -150,8 +181,24 @@ const ProfilePage = () => {
                 {/*change location to passable {variable} later */}
                 <p className="text-gray-800 text-lg mt-2">Location</p>
                 {/*change Bio/Stats to passable {variable} later */}
-                <p className="text-gray-500 text-sm mt-2">tell us about yourself!</p>
-                <p className="text-sm mb-2 text-gray-700 mt-4">playlists | reviews </p>
+                {isEditingBio ? (
+                  <input
+                    id="bioText"
+                    type="text"
+                    className="border border-gray-300 text-sm px-2 bg-white rounded-md p-2 mt-2 mb-2"
+                    value={bioText}
+                    placeholder="Tell us about yourself!"
+                    onChange={(e) => setBioText(e.target.value)}
+                    onKeyDown={handleKeyDown} // Call handleKeyDown when a key is pressed
+                  />
+                ) : (
+                  <p className="text-gray-500 text-sm mt-2">{bioText}</p>
+                )}
+              <div className="flex flex-row text-gray-500 text-sm">
+                {!isEditingBio ? ("Tell us about yourself") : ("")}
+                <CiEdit className="ml-2 hover:cursor-pointer" onClick={toggleEditMode} />
+                </div>
+                <p className="text-sm mb-2 text-orange mt-7 ">playlists | reviews </p>
                 
               </div>
             </div>
@@ -159,7 +206,7 @@ const ProfilePage = () => {
             {/*bottom*/}
             
              {/*Playlists*/}
-             <div className="flex-row mt-10">
+             <div className="flex-row mt-10 text-lg">
               Your Playlists
               
               <div className="border border-2 rounded-md p-4 mt-3">
@@ -170,7 +217,7 @@ const ProfilePage = () => {
             
 
             {/*reviews*/}
-            <div className="flex-row mt-10">
+            <div className="flex-row mt-10 text-lg">
               Your Reviews
               <div className="bg-gray-200 border p-4 mt-3">
                 render reviews component by user
