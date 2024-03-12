216d654a7addc644613231e0cf4ab89a03944832
aparnahrn <aparnahariharan@ucla.edu>
2024-03-12 13:59:36 -0700

    Update AboutMe.tsx

---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+import { useNavigate } from "react-router-dom";
+
+const AboutMe = () => {
+    const navigate = useNavigate();
+
+    return (
+        <NavBar />
+    )
+}
