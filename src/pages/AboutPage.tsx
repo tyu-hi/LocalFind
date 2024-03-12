95c920c8cf6215b52a6d99d221c412838641ded7
tyu-hi <tyugo2122@gmail.com>
2024-03-11 21:35:20 -0700

    about page

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <NavBar/>
+
+        AboutPage
+    </div>
+  )
+}
+
+export default AboutPage
